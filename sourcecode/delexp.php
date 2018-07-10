a854de432fcdf2af239f152fbadd51e52994b32c
navaneeth <navaneethbhasuram@gmail.com>
2018-07-10 23:14:14 +0530

    version 3.0

--- sourcecode/delexp.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$comp=$_GET['comp'];
-$pos=$_GET['pos'];
-$id=$_SESSION['id'];
-$connect=mysqli_connect("localhost","root","","job");
-$query="delete from experience where company='$comp' and position='$pos' and logid=$id";
-$output=mysqli_query($connect,$query);
-?>
--- sourcecode/delskill.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$skill=$_GET['remove'];
-$id=$_SESSION['id'];
-$connect=mysqli_connect("localhost","root","","job");
-$query="delete from skills where skill='$skill' and logid=$id";
-$output=mysqli_query($connect,$query);
-?>
